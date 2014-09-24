b8d727f2ad57ab02e1ddb0f920f4b347aff2c665
Jonathan P. Chen <jonathanp.chen@gmail.com>
2014-09-23 23:46:25 -0400

    Added artwork

--- adventure-game.sh
+++ adventure-game.sh
@@ -16,7 +16,14 @@ You look around you. It looks pretty barren. There's giant R inscribed in the wa
 You walk up to a large table in the middle of the room.
 There are three Pokeballs.
 What do you do?"
+win=$"
+You have defeated the great Giovanni
+All of the Pokemon world has you to thank.
+Horray!!
+"
 
+
+cat "$DIR/img/pokemonlogo.jpc"
 echo "Welcome to the Labyrinth Adventure Game!  You can leave anytime by typing [exit]"
 echo "Type [help] to see instructions"
 echo "Type [start] to begin!"
@@ -55,15 +62,38 @@ prompt () {
     fi
 }
 
+win () {
+    echo "Giovani: Hahaha nice try! You have defeated me this time, but I will be back!"
+    sleep 1
+    echo "Huh where did he go??"
+    sleep 1
+    echo ...
+    sleep 1
+    echo "Well at least I got rid of him and saved the world. Mom would be proud!"
+    sleep 1
+    cat "$DIR/img/congrats.jpc"
+    echo $winmessage
+    sleep 1
+    cat "$DIR/img/pikachu.jpc"
+    sleep 1
+    cat "$DIR/img/bulb.jpc"
+    sleep 1
+    cat "$DIR/img/squirtle.jpc"
+    sleep 1
+    cat "$DIR/img/charizard.jpc"
+    exit
+}
+
 battle () {
 #battle with opponent
     ehealth=40  #enemy health
-    if [[ "$1" = "boss" ]]
+    if [[ "$1" = "boss.jpc" ]]
     then
             echo "The Team Rocket boss wants to fight!"
             ehealth=80
     else echo "Team Rocket wants to fight!"
     fi
+    sleep 1
     echo "Team Rocket sends $(cat $1)!"
     echo "Go $pick!!"
     emonster=$(cat $1)
@@ -82,6 +112,7 @@ battle () {
             *) echo "Please pick (1) (2) (3)" ;;
         esac
         echo "Team Rocket used $(head -n 1 $DIR/monsters/$emonster.jpc)!"
+        sleep 1
         echo "Ouch! You lost 20 health."
         ((health-=20))
         if [[ $(( RANDOM % 2 )) -eq 0 && "$1" = "rocket2.jpc" ]]
@@ -89,19 +120,27 @@ battle () {
             then
                 poisoned=true
                 echo "$pick has been poisoned! Each time you move to a new room, you will lose 10 health!"
+                sleep 1
         fi
     echo Your health: $health
     echo Enemy health: $ehealth
+    sleep 1
     if [[ $ehealth -le 0 ]]
         then echo You defeated Team Rocket!
     fi
     done
+    if [[ "$1" = "boss.jpc" ]]
+        then win
+    fi
 }
 
 move () {
     if [[ $(cd $1) -eq 0 && -d $1 ]]
         then cd $1
             echo "You are now $1"
+    elif [[ $1 = "ground" || ! -d * ]]
+        then cd ..
+            echo "You have left the room"
     else
         echo "Location not recognized. Please enter a valid location."
         return
@@ -112,7 +151,7 @@ move () {
             echo "$pick is poisoned! Your health is now $health"
             echo "You should probably find an antidote!"
     fi
-    if [[ -f "rocket1.jpc"="true" ]]
+    if [[ -f "rocket1.jpc" ]]
         then battle rocket1.jpc
     elif [[ -e "rocket2.jpc" ]]
         then battle rocket2.jpc
@@ -121,12 +160,12 @@ move () {
     elif [[ -e "boss.jpc" ]]
         then battle boss.jpc
     fi
-    echo $1
 }
 
 #Main game loop
 while [[ $health -gt 0 && $leave != true ]]
 do
+    win
     printf ">> "
     read cmd location
     if [[ "$cmd" = "exit" ]]
