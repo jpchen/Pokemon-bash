130bea9213570a0158fc9b3b4c330b56acb07c90
Jonathan P. Chen <jonathanp.chen@gmail.com>
2014-09-23 22:13:36 -0400

    got the navigating rooms working

--- adventure-game.sh
+++ adventure-game.sh
@@ -4,6 +4,7 @@ health=200
 leave=false
 location=start
 pick=""
+poisoned=false
 enemy=rocket1.jpc
 items={}
 instr=$"Instructions:
@@ -11,7 +12,7 @@ go-to [room] takes you to the room
 [1-5] corresponds to choice from prompt
 exit - exits the game"
 opening=$"You wake up in a large, opulent mansion.
-you look around you. It looks pretty barren.
+You look around you. It looks pretty barren. There's giant R inscribed in the walls. 
 You walk up to a large table in the middle of the room.
 There are three Pokeballs.
 What do you do?"
@@ -21,6 +22,7 @@ echo "Type [help] to see instructions"
 echo "Type [start] to begin!"
 
 begin () {
+    cd labyrinth/ground/
     echo "$opening"
     echo 1. Pick the red one
     echo 2. Pick the blue one
@@ -43,7 +45,7 @@ battle () {
     ehealth=40  #enemy health
     if [[ "$1" = "boss" ]]
     then
-            echo "The boss wants to fight!"
+            echo "The Team Rocket boss wants to fight!"
             ehealth=80
     else echo "Team Rocket wants to fight!"
     fi
@@ -65,14 +67,43 @@ battle () {
             *) echo "Please pick (1) (2) (3)" ;;
         esac
         echo "Team Rocket used $(head -n 1 $DIR/monsters/$emonster.jpc)!"
-        echo "Ouch! You lost 20 health"
+        echo "Ouch! You lost 20 health."
         ((health-=20))
-    echo $health
-    echo $ehealth
+        if [[ $(( RANDOM % 2 )) -eq 0 && "$1" = "rocket2.jpc" ]]
+            # 50% chance of getting poisoned from Poison Powder
+            then
+                poisoned=true
+                echo "$pick has been poisoned! Each time you move to a new room, you will lose 10 health!"
+        fi
+    echo Your health: $health
+    echo Enemy health: $ehealth
     if [[ $ehealth -le 0 ]]
         then echo You defeated Team Rocket!
     fi
     done
+}
+
+move () {
+    if [[ $(cd $1) -ne 0 ]]
+        then echo "You are now $1"
+    else echo "Location not recognized. Please enter a valid location."
+    fi
+    if [[ "$poisoned" = true ]]
+        then
+            ((health-=10))
+            echo "$pick is poisoned! Your health is now $health"
+            echo "You should probably find an antidote!"
+    fi
+    if [ -e "rocket1.jpc" ]
+        then battle rocket1.jpc
+    elif [ -e "rocket2.jpc" ]
+        then battle rocket2.jpc
+    elif [ -e "rocket3.jpc" ]
+        then battle rocket3.jpc
+    elif [ -e "boss.jpc" ]
+        then battle boss.jpc
+    fi
+    echo $1
 }
 
 #Main game loop
@@ -89,19 +120,22 @@ do
         then echo "$instr"
     elif [[ "$cmd" = "go-to" ]]
         #move to different location
-        then echo "$location"
+        then move $location
+    else
+        echo "Sorry, command not recognized. To move to a different room, type: go-to [room]"
+    fi
+    printf 'You are here: %s\n' "${PWD##*/}"
+    echo Where do you want to go?
+    for i in $(ls -d */)
+    do
+        echo $i 
+    done
+    parent=$(dirname $PWD)
+    if [[ $(basename ${PWD##*/}) != "ground" ]]
+        # dont show parent directory as option if on ground floor
+        then echo $(basename $parent) #go back from whence you came
     fi
 
-    cd labyrinth/ground
-    if [ -e "rocket1.jpc" ]
-        then battle rocket1.jpc
-    elif [ -e "rocket2.jpc" ]
-        then battle rocket2.jpc
-    elif [ -e "rocket3.jpc" ]
-        then battle rocket3.jpc
-    elif [ -e "boss.jpc" ]
-        then battle boss.jpc
-    fi
 done
 
 
